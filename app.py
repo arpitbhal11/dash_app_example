f263af7de59e6d783cd24a5579515cb54438883e
arpitbhal11 <arpitkumar.bhal@alumni.esade.edu>
2019-11-25 15:55:17 +0100

    third

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash(app)
+app = dash.Dash(__name__)
 server=app.server
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
